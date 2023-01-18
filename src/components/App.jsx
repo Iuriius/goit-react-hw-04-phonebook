cec8d2731f41ee230025fb481aab33e4ff1bbd19
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-01-18 19:40:43 +0200

    effect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import { nanoid } from 'nanoid';
 import { Section, Title } from './App.styled';
@@ -6,77 +6,61 @@ import Form from './Form';
 import ContactsList from './ContactsList';
 import Filter from './Filter';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const data = JSON.parse(localStorage.getItem('contacts'));
-    if (data) {
-      this.setState({ contacts: data });
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  componentDidUpdate(_, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  addNewContact = data => {
-    const { contacts } = this.state;
+  function addNewContact(data) {
     const newContact = {
       id: nanoid(),
       ...data,
     };
+    toast.success(`${data.name} added`);
     contacts.some(({ name }) => name === data.name)
       ? toast.error(`${data.name} is already in contacts`)
       : this.setState(prevState => ({
           contacts: [...prevState.contacts, newContact],
         }));
+  }
+
+  const deleteContact = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
   };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const changeFilter = event => {
+    setFilter(event.currentTarget.value);
   };
 
-  changeFilter = event => {
-    this.setState({ filter: event.currentTarget.value });
-  };
-
-  getContacts = () => {
-    const { filter, contacts } = this.state;
+  function getContacts() {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(({ name }) =>
       name.toLowerCase().includes(normalizedFilter)
     );
-  };
-
-  render() {
-    const { filter } = this.state;
-    const currentContacts = this.getContacts();
-    return (
-      <>
-        <div>
-          <Toaster />
-        </div>
-        <Section>
-          <Title>Phonebook</Title>
-          <Form addNewContact={this.addNewContact} />
-        </Section>
-        <Section>
-          <Title>Contacts</Title>
-          <Filter value={filter} onChange={this.changeFilter} />
-          <ContactsList
-            contacts={currentContacts}
-            deleteContact={this.deleteContact}
-          />
-        </Section>
-      </>
-    );
   }
+
+  const currentContacts = getContacts();
+
+  return (
+    <>
+      <div>
+        <Toaster />
+      </div>
+      <Section>
+        <Title>Phonebook</Title>
+        <Form addNewContact={addNewContact} />
+      </Section>
+      <Section>
+        <Title>Contacts</Title>
+        <Filter value={filter} onChange={changeFilter} />
+        <ContactsList
+          contacts={currentContacts}
+          deleteContact={deleteContact}
+        />
+      </Section>
+    </>
+  );
 }
